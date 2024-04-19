029d571ae8447bb752d8423fc0a2b7f5fa0b468f
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-04-19 11:33:45 -0500

    Update eval.sh

--- experiment/eval.sh
+++ experiment/eval.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 export cwd=$(pwd)
-GIT_SSH_COMMAND="ssh -i ${cwd}/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone git@github.com:feji3769/chtc.git project
+
+GIT_SSH_COMMAND="ssh -i ${cwd}/id_ed25519 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git clone --single-branch --branch jb git@github.com:feji3769/chtc.git project
+
+# simlink
 ln -s ${cwd}/* /workspace/
 
-cp /staging/fjimenez2/something.txt .
+cp /staging/brachem/something.txt .
 
 /opt/conda/bin/python3 /workspace/project/experiments/eval.py \
     --seed=$1
