f401a6b4b96774f3568d59656be93143f1889911
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-04-19 11:33:41 -0500

    Update build.sh

--- container/build.sh
+++ container/build.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 docker build \
-    -t felixj3/pskips:nothing.0.0.0 \
+    -t jobrachem/chtc_test:image.1 \
     --no-cache\
-    --build-arg UID=25666 \
-    --build-arg GID=25666 \
-    --build-arg DOCKER_USER=fjimenez2 \
-    --build-arg GIT_EMAIL="felix.m.jimenez3@gmail.com" \
-    --build-arg GIT_USERNAME=feji3769 .
+    --build-arg UID=26064 \
+    --build-arg GID=26064 \
+    --build-arg DOCKER_USER=jobrachem \
+    --build-arg GIT_EMAIL="jobrachem@posteo.de" \
+    --build-arg GIT_USERNAME=jobrachem .
